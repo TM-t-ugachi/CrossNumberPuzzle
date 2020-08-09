6af15dc7df8824705ec8c79081aec84f0b1b8a4c
MYCOMPUTER\nwuser <taisei.rock.metal@gmail.com>
2020-08-09 12:20:03 +0900

    ディレクトリ構成をMVP向けに変更

--- app/src/main/java/com/example/crossnumberpuzzle/presentation/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/crossnumberpuzzle/presentation/view/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.crossnumberpuzzle
+package com.example.crossnumberpuzzle.presentation.view.activity
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.crossnumberpuzzle.R
 
 class MainActivity : AppCompatActivity() {
 
